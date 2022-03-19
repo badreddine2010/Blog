5c1908e2b14a1e0a6f94d237de51d831026747b7
Badreddine <bdjaiz@hotmail.com>
2022-03-19 02:48:00 +0100

    afficher les article/article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="app_blog")
+     * @Route("/blog/contact", name="blog-contact")
      */
-    public function index(): Response
+    public function contact(): Response
     {
+        return $this->render('blog/contact.html.twig', [
+            // 'controller_name' => 'BlogController',
+        ]);
+    }
+
+ /**
+     * @Route("/", name="home")
+     */
+    public function index(ArticleRepository $articleRepo): Response
+    {
+        $articles = $articleRepo->findAll();
+        // dd($articles); //dump and die
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles'=> $articles
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="art-detail")
+     */
+    public function artDetail(Article $article): Response
+    {
+        // $articles = $articleRepo->findAll();
+        // dd($article); //dump and die
+        return $this->render('blog/artDetail.html.twig', [
+            'article'=> $article
         ]);
     }
 }
